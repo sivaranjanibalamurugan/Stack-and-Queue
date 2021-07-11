81fc5affc4fe403c46d780e591af43be60e57d21
unknown <sivaranjanib99@gmail.com>
2021-07-11 18:09:35 +0530

    [siva] welcome to Stack and Queue

---
+++ Stack and Queue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stack_and_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to stack and queue!!");
+        }
+    }
+}
